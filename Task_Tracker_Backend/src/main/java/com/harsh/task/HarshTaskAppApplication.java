e9f4c1392afb9ec47d794afeb4b8a080abbe1e0d
Harshvardhan <harshvardhanrokade845@gmail.com>
2026-04-09 13:28:21 +0530

    Timezone error for reminder

--- Task_Tracker_Backend/src/main/java/com/harsh/task/HarshTaskAppApplication.java
+++ Task_Tracker_Backend/src/main/java/com/harsh/task/HarshTaskAppApplication.java
@@ -3,14 +3,20 @@ package com.harsh.task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class HarshTaskAppApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(HarshTaskAppApplication.class, args);
 	}
 
+	// This forces the Spring Boot app to run in IST no matter the server!
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+	}
 }
