b35509d4f22d06fbc61a0176789e426d40c1eefa
Harshvardhan <harshvardhanrokade845@gmail.com>
2026-04-11 08:05:56 +0530

    change

--- Task_Tracker_Backend/src/main/java/com/harsh/task/security/SecurityConfig.java
+++ Task_Tracker_Backend/src/main/java/com/harsh/task/security/SecurityConfig.java
@@ -67,7 +67,6 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        
         config.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
                 "https://*.vercel.app",
